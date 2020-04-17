0c6159e3cc51c00be030fe09d0e059b01d99cd19
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-04-17 16:46:18 -0400

    externalized neopixel code since it needs to be run standalone as sudo

---
+++ RaspberryPi/neopix.py
@@ -0,0 +1,23 @@
+import sys
+import board
+import neopixel
+import random
+
+# Change to the number of LEDs you are using
+LED_COUNT = 142
+pixels = neopixel.NeoPixel(board.D18, LED_COUNT)
+
+try:
+    led_status = sys.argv[1]
+except:
+    print("Missing LED_STATUS argument")
+    sys.exit()
+
+if led_status == 'on':
+    for x in range(0, LED_COUNT):
+        pixels[x] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+elif led_status == "off":
+    for x in range(0, LED_COUNT):
+        pixels[x] = (0, 0, 0)
+else:
+    print("Unknown state")
