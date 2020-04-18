6f7160ae9e17e7060d0454065512b9757b8c3781
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-04-17 22:35:55 -0400

    Removed random print statement and moved comment about verbose tasklist to config.py

--- Windows/aiim/check_proc.py
+++ Windows/aiim/check_proc.py
@@ -5,11 +5,9 @@ import config
 
 
 def find_processes():
-    #  Need the /v for verbose. /fi filters the process and looks for window title for Teams meeting
     p = os.popen(config.tasklist_query()).read().splitlines()
     is_it_running = 0
     for item in p:
-        print(item)
         # Still needs condition because otherwise the console returns "No tasks are running for specific criteria"
         if config.ZOOM is True and item.find('CptHost.exe') > -1:
                 is_it_running += 1
