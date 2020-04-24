a9f105585a6eadd07a0b62b4c559d052b7f864cb
Nazmus Nasir <nazmus.nasir@gmail.com>
2020-04-24 00:00:38 -0400

    Looking for themes in the input

--- RaspberryPi/neopix.py
+++ RaspberryPi/neopix.py
@@ -2,6 +2,7 @@ import sys
 import board
 import neopixel
 import random
+import themes
 
 # Change to the number of LEDs you are using
 LED_COUNT = 142
@@ -14,9 +15,35 @@ except:
     print("Missing LED_STATUS argument")
     sys.exit()
 
+try:
+    theme = sys.argv[2]
+except:
+    theme = 'random'
+
+
+def theme_colors():
+    if theme == 'random':
+        return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+    elif theme == 'sunset':
+        sunset_size = len(themes.sunset)
+        # return random.randint(0, 255), random.randint(0, 128), random.randint(0, 36)
+        chosen_color = themes.sunset[random.randint(0, sunset_size)-1]
+        return chosen_color[0], chosen_color[1], chosen_color[2]
+    elif theme == 'cool':
+        cool_size = len(themes.cool)
+        chosen_color = themes.cool[random.randint(0, cool_size)-1]
+        return chosen_color[0], chosen_color[1], chosen_color[2]
+    elif theme == 'nature':
+        nature_size = len(themes.nature)
+        chosen_color = themes.nature[random.randint(0, nature_size)-1]
+        return chosen_color[0], chosen_color[1], chosen_color[2]
+    else:
+        print("Uknown theme. Choose from sunset, cool, and nature only")
+
+
 if led_status == 'on':
     for x in range(0, LED_COUNT):
-        pixels[x] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        pixels[x] = (theme_colors())
 elif led_status == "off":
     for x in range(0, LED_COUNT):
         pixels[x] = (0, 0, 0)
